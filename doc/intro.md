38102ef3d0f32cb9dbd96a0ee70a0f64b919de62
Kiran Karkera <kiran.karkera@gmail.com>
2017-05-26 11:32:28 +0800

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to cortex-tensorboard-viewer
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/cortex_tensorboard_viewer/core_test.clj
@@ -0,0 +1,7 @@
+(ns cortex-tensorboard-viewer.core-test
+  (:require [clojure.test :refer :all]
+            [cortex-tensorboard-viewer.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
