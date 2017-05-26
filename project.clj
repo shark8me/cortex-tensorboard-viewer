fe1965971fad9f66b45878cdcef0afa175975597
Kiran Karkera <kiran.karkera@gmail.com>
2017-05-26 18:30:08 +0800

    moved some code to core ns

--- project.clj
+++ project.clj
@@ -1,6 +1,11 @@
 (defproject cortex-tensorboard-viewer "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Enables the monitoring (using Tensorboard) of neural networks training with Cortex "
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.shark8me/tfevent-sink "0.1.2-SNAPSHOT"]
+                 [thinktopic/cortex "0.9.9-SNAPSHOT"]
+                 ;;build off the https://github.com/thinktopic/cortex/pull/172 branch
+                 [thinktopic/experiment "0.9.10-SNAPSHOT"]
+                 ])
--- src/cortex_tensorboard_viewer/core.clj
+++ src/cortex_tensorboard_viewer/core.clj
@@ -1,6 +1,27 @@
-(ns cortex-tensorboard-viewer.core)
+(ns cortex-tensorboard-viewer.core
+  (:require [clojure.java.io :as io]
+            [tfevent-sink.event-io :as eio]
+            [clojure.core.async :as async :refer [>! <! <!! >!! chan close! go alts!]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn tf-event-sink
+  "starts appending tensorflow events to file-path. num-msgs messages are read from
+  the core.async channel. returns a core.async channel"
+  [file-path channel num-msgs]
+  (do
+    (eio/create-event-stream file-path)
+    (go (dotimes [n num-msgs]
+          (let [[v _] (alts! [channel])]
+            (eio/append-events file-path v))))))
+
+(defn scalar-event-xform [prefix]
+  "returns a xform function that creates scalar tensorflow events. The prefix argument 
+  is the name prefix for the metric being reported"
+  (fn [xf]
+    (fn
+    ;; START
+      ([]          (xf))
+    ;; STOP
+      ([result]    (xf result))
+    ;; PROCESS
+      ([result input]
+       (xf result (mapv (fn [[k v]] (eio/make-event (str prefix (name k)) v)) input))))))
